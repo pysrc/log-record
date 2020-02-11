e83c685e39246fa43a96d6f95635b51def4f6bcd
L Chen <1570184051@qq.com>
2020-02-11 18:42:41 +0800

    新增相同服务日志集中

--- readme.md
+++ readme.md
@@ -10,3 +10,9 @@ example:
 
 只需要将待收集的日志输出管道输出到收集客户端即可，客户端会同步到服务端
 
+### 相同服务日志集中
+
+`http://127.0.0.1:9587/svc?svc=ClientSystem`
+
+在server上即可集中ClientSystem服务的日志ClientSystem.log
+
--- server/LogServer.go
+++ server/LogServer.go
@@ -1,15 +1,78 @@
 package main
 
 import (
+	"bufio"
 	"encoding/base64"
 	"flag"
-	"log"
+	"fmt"
 	"net/http"
+	"os"
 )
 
+type LogInfo struct {
+	Svc  string
+	Info string
+}
+
+type LogWriter struct {
+	Svc    string
+	File   *os.File
+	Writer *bufio.Writer
+}
+
+var logs chan *LogInfo
+var svcs []*LogWriter
+
+func Handle() {
+	for {
+		ilog := <-logs
+		fmt.Println(ilog.Svc, ilog.Info)
+		for _, svc := range svcs {
+			if ilog.Svc == svc.Svc {
+				svc.Writer.WriteString(ilog.Info + "\n")
+				svc.Writer.Flush()
+			}
+		}
+	}
+}
+
+func AddSvc(svc string) string {
+	for _, v := range svcs {
+		if v.Svc == svc {
+			return ""
+		}
+	}
+	var wt LogWriter
+	file, err := os.OpenFile(svc+".log", os.O_WRONLY|os.O_CREATE, 0644)
+	if err != nil {
+		return err.Error()
+	}
+	wt.Svc = svc
+	wt.File = file
+	wt.Writer = bufio.NewWriter(file)
+	svcs = append(svcs, &wt)
+	return "Success !"
+}
+
 func main() {
+
 	var host = flag.String("h", "0.0.0.0:9587", "服务地址")
+	var size = flag.Int("s", 100, "日志缓存大小")
 	flag.Parse()
+	logs = make(chan *LogInfo, *size)
+	go Handle()
+	defer func() {
+		for _, v := range svcs {
+			v.Writer.Flush()
+			v.File.Close()
+		}
+	}()
+	// 新增一个服务日志输出
+	http.HandleFunc("/svc", func(w http.ResponseWriter, r *http.Request) {
+		svc := r.FormValue("svc")
+		w.Write([]byte(AddSvc(svc)))
+	})
+	// 接收日志推送
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		svc := r.FormValue("svc")
 		info := r.FormValue("info")
@@ -17,10 +80,10 @@ func main() {
 		if err != nil {
 			return
 		}
-		log.Println(svc, string(dec))
+		logs <- &LogInfo{svc, string(dec)}
 	})
 	var err = http.ListenAndServe(*host, nil)
 	if err != nil {
-		log.Panic(err)
+		panic(err)
 	}
 }
